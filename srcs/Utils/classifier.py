eb31030a968ff3b594adc57cfbab5ea1c67099fb
samusanc <samu2004ssmm@gmail.com>
2025-12-15 22:48:40 +0100

    feat(major structure)

--- srcs/Utils/classifier.py
+++
@@ -1,9 +0,0 @@
-
-
-i need a program that takes a directory and make a csv from that folder
-
-the csv will contain the following
-
-path, tag, decease, split
-
-"./path/", apple, decease, 
--- srcs/Utils/dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find ./images -type f | while read -r file; do
-    type=$(echo "$file" | tr '/' ' ' | awk '{print $3}')
-    echo "$file, $type"
-done
-
